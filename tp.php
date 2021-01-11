6630b5a2bd4f532dbfa1ec77fcdfa7b1c315d018
bakeze <71679309+bakeze@users.noreply.github.com>
2021-01-11 21:04:33 +0000

    Update tp.php

--- tp.php
+++ tp.php
@@ -1,1 +1,12 @@
-h
+<?php
+$coter_carrer = 4;
+$perimettre_du_carrer = $coter_carrer * 4;
+$demi_perimetre =  $coter_carrer * 2;
+$longueur_diagonnale = $coter_carrer * sqrt(2);
+
+echo "<big>LES CALCUL DANS UN CARRER <big/> <br/>";
+echo "le coter du carrer est : $coter_carrer <br/>";
+echo "le perimetre du carrer est : $perimettre_du_carrer <br/>";
+echo "le demi_ erimetre du carrer est : $demi_perimetre <br/>";
+echo "une longueur_diagonnale du carrer est : $longueur_diagonnale <br/>";
+?>
